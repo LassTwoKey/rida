0d0494f1a926bfc1a356d001ab96d230cc8af3ae
dkn1fe <dknifeb@mail.ru>
2025-06-29 21:50:22 +0300

    test

---
+++ src/App.NextApp/shared/components/container/container.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+const Container = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className='max-w-[1440px] m-auto py-10'>{children}</div>
+    )
+}
+
+export default Container;

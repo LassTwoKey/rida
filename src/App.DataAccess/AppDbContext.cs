f16e415b636076c1ce985bd4589cc848754172fc
LassTwoKey <dimas.super29@gmail.com>
2025-07-04 18:13:20 +0300

    dfvsfsdf

--- src/App.DataAccess/AppDbContext.cs
+++ src/App.DataAccess/AppDbContext.cs
@@ -27,12 +27,5 @@ namespace App.DataAccess
                 optionsBuilder.UseNpgsql(_configuration.GetConnectionString(nameof(AppDbContext)));
             }
         }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.HasDefaultSchema("public");
-
-            base.OnModelCreating(modelBuilder);
-        }
     }
 }

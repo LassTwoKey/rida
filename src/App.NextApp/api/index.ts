b5a350c30e195e6e89e982abe46cbfa7d175ec2d
LassTwoKey <dimas.super29@gmail.com>
2025-07-06 00:55:15 +0300

    change in configs

--- src/App.NextApp/api/index.ts
+++ src/App.NextApp/api/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const CoreAbortController = new AbortController();
 
-const baseURL = process.env.NODE_ENV === "production" ? "localhost:8080" : "147.78.66.97";
+const baseURL = process.env.NODE_ENV === "production" ? window.location.host : "147.78.66.97";
 
 const CoreHttp = axios.create({
     withCredentials: true,
